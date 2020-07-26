dbe522cc6ac2067b5edc175cc79492b3ca2d647d
Pratyaksh Tyagi <55044774+pratyaksh123@users.noreply.github.com>
2020-07-27 04:41:04 +0530

    Added FAB.Group

--- App/Components/FAB.js
+++ App/Components/FAB.js
@@ -1,24 +1,47 @@
 import * as React from 'react'
-import { StyleSheet } from 'react-native'
-import { FAB } from 'react-native-paper'
+import { FAB, Portal } from 'react-native-paper'
 
-const styles = StyleSheet.create({
-  fab: {
-    position: 'absolute',
-    margin: 25,
-    right: 0,
-    bottom: 0,
-  },
-})
+const Fab = () => {
+  const [state, setState] = React.useState({ open: false })
 
-const Fab = () => (
-  <FAB
-    style={styles.fab}
-    large
-    icon="camera"
-    // eslint-disable-next-line no-console
-    onPress={() => console.log('Pressed')}
-  />
-)
+  const onStateChange = ({ open }) => setState({ open })
+
+  const { open } = state
+  return (
+    <>
+      <Portal>
+        <FAB.Group
+          open={open}
+          icon={open ? 'close' : 'camera'}
+          actions={[
+            {
+              icon: 'star',
+              label: 'Star',
+              // eslint-disable-next-line no-console
+              onPress: () => console.log('Pressed star'),
+            },
+            {
+              icon: 'email',
+              label: 'Email',
+              // eslint-disable-next-line no-console
+              onPress: () => console.log('Pressed email'),
+            },
+            {
+              icon: 'bell',
+              label: 'Remind',
+              onPress: () => console.log('Pressed notifications'),
+            },
+          ]}
+          onStateChange={onStateChange}
+          onPress={() => {
+            if (open) {
+              // do something if the speed dial is open
+            }
+          }}
+        />
+      </Portal>
+    </>
+  )
+}
 
 export default Fab
