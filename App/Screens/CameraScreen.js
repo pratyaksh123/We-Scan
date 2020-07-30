58dd44000e9cb2f96a7797ae8d436c81bf4c1d5f
Pratyaksh Tyagi <55044774+pratyaksh123@users.noreply.github.com>
2020-07-30 22:58:07 +0530

    Improved UI

--- App/Screens/CameraScreen.js
+++ App/Screens/CameraScreen.js
@@ -5,9 +5,15 @@ import {
   ActivityIndicator,
   StyleSheet,
   Dimensions,
+  Alert,
 } from 'react-native'
 import { Camera } from 'expo-camera'
 import { DarkTheme, FAB } from 'react-native-paper'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const styles = StyleSheet.create({
   container: {
@@ -17,9 +23,11 @@ const styles = StyleSheet.create({
     height: 50,
     width: Dimensions.get('screen').width,
     backgroundColor: DarkTheme.colors.background,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   bottomCameraView: {
-    height: 100,
+    height: 90,
     width: Dimensions.get('screen').width,
     backgroundColor: DarkTheme.colors.background,
   },
@@ -30,8 +38,26 @@ const styles = StyleSheet.create({
   },
   fab: {
     position: 'absolute',
-    right: Dimensions.get('screen').width / 2,
-    bottom: 0,
+    left: Dimensions.get('screen').width / 2 - 28, // Width of a FAB is 56
+    bottom: 17,
+  },
+  Icon: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bottomBarIcons: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  captureTypeButtons: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: 30,
   },
 })
 
@@ -59,15 +85,52 @@ export default function CameraScreen() {
   }
   return (
     <View style={styles.container}>
-      <Camera ref={cameraRef} style={styles.cameraView} type={type}>
-        <View style={styles.upperCameraView} />
+      <Camera
+        ref={cameraRef}
+        style={styles.cameraView}
+        onMountError={(err) => {
+          // TODO make a ErrorString Util
+          Alert.alert('error', err.message)
+        }}
+        type={type}
+      >
+        <View style={styles.upperCameraView}>
+          <View style={styles.Icon}>
+            <FontAwesome name="flash" color="white" size={30} />
+          </View>
+          <View style={styles.Icon}>
+            <FontAwesome5 name="portrait" color="white" size={30} />
+          </View>
+          <View style={styles.Icon}>
+            <MaterialIcons name="hd" color="white" size={30} />
+          </View>
+          <View style={styles.Icon}>
+            <MaterialCommunityIcons
+              name="dots-vertical"
+              color="white"
+              size={30}
+            />
+          </View>
+        </View>
         <View style={styles.bottomCameraView}>
-          <FAB
-            style={styles.fab}
-            large
-            icon="camera"
-            onPress={cameraButtonPressed}
-          />
+          <FAB style={styles.fab} icon="camera" onPress={cameraButtonPressed} />
+          <View style={styles.bottomBarIcons}>
+            <View style={styles.Icon}>
+              <MaterialCommunityIcons
+                name="image-area"
+                color="white"
+                size={25}
+              />
+            </View>
+            <View style={styles.captureTypeButtons}>
+              <View style={styles.Icon}>
+                <Ionicons name="document" color="white" size={25} />
+              </View>
+              <View style={styles.Icon}>
+                <Ionicons name="documents-sharp" color="white" size={25} />
+              </View>
+            </View>
+          </View>
         </View>
       </Camera>
     </View>
